109d63cfa66552eba62bf3dafda1f20db44587bf
wooway777 <wooway777@gmail.com>
2025-05-16 14:38:43 +0800

    Elementwise Support for Cambricon Bang

---
+++ src/infiniop/devices/bang/bang_kernel_common.h
@@ -0,0 +1,41 @@
+#ifndef __INFINIOP_BANG_KERNEL_COMMON_H__
+#define __INFINIOP_BANG_KERNEL_COMMON_H__
+
+#include "cnnl.h"
+#include "cnrt.h"
+
+namespace device::bang::kernel {
+
+inline __mlu_device__ size_t indexToReducedOffset(
+    size_t flat_index,
+    size_t ndim,
+    const ptrdiff_t *broadcasted_strides,
+    const ptrdiff_t *target_strides) {
+
+    size_t res = 0;
+    for (size_t i = 0; i < ndim; ++i) {
+        res += flat_index / broadcasted_strides[i] * target_strides[i];
+        flat_index %= broadcasted_strides[i];
+        __sync_all();
+    }
+    return res;
+}
+
+inline __mlu_device__ size_t indexToOffset(
+    size_t flat_index,
+    size_t ndim,
+    const size_t *shape,
+    const ptrdiff_t *strides) {
+
+    size_t res = 0;
+    for (size_t i = ndim; i-- > 0;) {
+        res += (flat_index % shape[i]) * strides[i];
+        flat_index /= shape[i];
+        __sync_all();
+    }
+    return res;
+}
+
+} // namespace device::bang::kernel
+
+#endif
