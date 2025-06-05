8ed1c588a53f4ade222e01ab6c59b9287c032421
wooway777 <wooway777@gmail.com>
2025-06-05 21:06:31 +0800

    Cambricon Reduce - RMS Norm

--- src/infiniop/devices/bang/common_bang.h
+++ src/infiniop/devices/bang/common_bang.h
@@ -9,6 +9,9 @@
 #include "cnrt.h"
 #include <functional>
 
+#define REM_FOR_STACK 8
+#define MAX_NRAM_SIZE (__MLU_NRAM_SIZE__ - REM_FOR_STACK)
+
 #define CHECK_BANG(API) CHECK_INTERNAL(API, CNNL_STATUS_SUCCESS)
 
 namespace device::bang {
@@ -16,11 +19,19 @@ namespace device::bang {
 class Handle::Internal {
     Pool<cnnlHandle_t> cnnl_handles;
 
+    int _core_per_cluster;
+    int _cluster_count;
+
     template <typename T>
     using Fn = std::function<infiniStatus_t(T)>;
 
 public:
+    Internal(int);
+
     infiniStatus_t useCnnl(cnrtQueue_t queue, const Fn<cnnlHandle_t> &f) const;
+
+    int getCorePerCluster() const;
+    int getClusterCount() const;
 };
 
 cnnlDataType_t getCnnlDtype(infiniDtype_t dt);
